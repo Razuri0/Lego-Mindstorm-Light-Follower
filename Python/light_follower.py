c83e32c8fbb236ed1c95c63d35575dbec5dc1400
dr254 <dr254@emai.uni-freiburg.de>
2025-10-09 01:14:13 +0200

    improved parity

--- Python/light_follower.py
+++ Python/light_follower.py
@@ -22,22 +22,26 @@ from ev3dev2.sensor.lego import LightSensor
 LIGHT_LEFT = LightSensor(INPUT_1)
 LIGHT_RIGHT = LightSensor(INPUT_2)
 MOTOR = LargeMotor(OUTPUT_A)
-P_TERM = 1.0
+P_TERM: float = 1.0
 
 
-def p_controller(sensor_left: LightSensor, sensor_right: LightSensor, p_term: float) -> float:
-    difference = sensor_left.reflected_light_intensity - sensor_right.reflected_light_intensity
-    return difference * p_term
+# p(roportional)-controller function
+def p_controller(sensor_left: int, sensor_right: int, p_term: float) -> float:
+    difference: float = sensor_left - sensor_right
+    return p_term * difference
 
 
 # is executed when the program is started
 if __name__ == "__main__":
 
     # variable to store p_controller output
-    motor_power = 0.0
+    motor_power: float
 
     # loops forever
     while True:
-        motor_power = p_controller(LIGHT_LEFT, LIGHT_RIGHT, P_TERM)
+        
+        motor_power = p_controller(LIGHT_LEFT.reflected_light_intensity, LIGHT_RIGHT.reflected_light_intensity, P_TERM)
         MOTOR.on(motor_power)
+
+        # wait for 100 milliseconds
         sleep(0.1)
